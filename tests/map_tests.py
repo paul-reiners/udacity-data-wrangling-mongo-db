2806c032854f0ce5ad9e0286eb6931cb3dc0ce4d
Paul Reiners <paul.reiners@gmail.com>
2014-12-30 14:38:56 -0600

    Adding a user test.

--- tests/map_tests.py
+++ tests/map_tests.py
@@ -2,8 +2,9 @@ from nose.tools import *
 import pprint
 import maps.mapparser
 import maps.tags
+import maps.users
 
-def test_count_tags():
+def test_mappparser_count_tags():
     tags = maps.mapparser.count_tags('./test-data/mapparser-example.osm')
     pprint.pprint(tags)
     assert {'bounds': 1,
@@ -15,7 +16,12 @@ def test_count_tags():
             'tag': 7,
             'way': 1} == tags
 
-def test_process_map():
+def test__tags_process_map():
     keys = maps.tags.process_map('./test-data/tags-example.osm')
     pprint.pprint(keys)
     assert keys == {'lower': 5, 'lower_colon': 0, 'other': 2, 'problemchars': 0}
+
+def test_users_process_map():
+    users = maps.users.process_map('./test-data/users-example.osm')
+    pprint.pprint(users)
+    assert len(users) == 4
